aa6cb6d1dc0e252840348404fa816f5e35a6da76
sgiehl <stefan@matomo.org>
2025-08-18 23:31:35 +0200

    fix tests for older releases

--- tests/System/AutoSuggestAPITest.php
+++ tests/System/AutoSuggestAPITest.php
@@ -57,6 +57,11 @@ class AutoSuggestAPITest extends SystemTestCase
 
         $xmlFieldsToRemove = ['latitude', 'longitude'];
 
+        if (version_compare(Version::VERSION, '5.4.0-b5', '<')) {
+            // In Matomo 5.4 ai referrer had been added
+            $xmlFieldsToRemove = array_merge($xmlFieldsToRemove, ['referrerAIAssistantUrl', 'referrerAIAssistantIcon']);
+        }
+
         $apiForTesting[] = [
             'Live.getLastVisitsDetails',
             [
--- tests/System/CustomVariablesSystemTest.php
+++ tests/System/CustomVariablesSystemTest.php
@@ -53,6 +53,11 @@ class CustomVariablesSystemTest extends SystemTestCase
             ];
         }
 
+        if (version_compare(Version::VERSION, '5.4.0-b5', '<')) {
+            // In Matomo 5.4 ai referrer had been added
+            $xmlFieldsToRemove = array_merge($xmlFieldsToRemove, ['referrerAIAssistantUrl', 'referrerAIAssistantIcon']);
+        }
+
         return [
             [
                 $apiToCall,
